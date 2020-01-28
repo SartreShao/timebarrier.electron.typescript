0444a18e1dd0748bc2b753af429c768e3473e742
邵励治 <shaolizhi1234@126.com>
2020-01-28 23:04:37 +0800

    INIT

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<style lang="stylus">
+#app {
+  width 100%
+  height 100%
+}
+body, h1, h2, h3, h4, h5, h6 {
+  margin 0
+  margin-block-start 0
+  margin-block-end 0
+  margin-inline-start 0
+  margin-inline-end 0
+}
+::-webkit-scrollbar {
+  display none
+}
+.el-message-box {
+  width 85% !important
+  max-width 420px
+}
+.el-button--primary {
+  background-color #3846CF !important
+  border-color #3846CF !important
+}
+.el-drawer__header {
+  margin-bottom 3.9vh !important
+}
+</style>
