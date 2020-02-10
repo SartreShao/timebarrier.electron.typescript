a6af72950f29829062f95764ea524d4034546e8d
邵励治 <shaolizhi1234@126.com>
2020-02-10 12:20:34 +0800

    添加了 LeanCloud 的依赖

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import VueCompositionApi from "@vue/composition-api";
+import * as AV from "leancloud-storage";
+
+AV.init({
+  appId: "1vrLSxhVS6DqUox0scqmyhCt-gzGzoHsz",
+  appKey: "ywrEEUSG5sE0OyMuXXvW7w8M",
+  serverURLs: "https://timebarrier.api.hearfresh.cn"
+});
 
 Vue.config.productionTip = false;
 
