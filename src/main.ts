df6ac8c196de502a45c99177335c63567de59c27
邵励治 <shaolizhi1234@126.com>
2020-03-21 11:54:49 +0800

    删除了 Vuex

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
 import VueCompositionApi from "@vue/composition-api";
 import * as AV from "leancloud-storage";
 import ElementUI from "element-ui";
@@ -20,6 +19,5 @@ Vue.use(ElementUI);
 
 new Vue({
   router,
-  store,
   render: h => h(App)
 }).$mount("#app");
