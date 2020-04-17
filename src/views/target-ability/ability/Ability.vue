eaedd87e064cb41a76fa921ed906c4a92c6facec
邵励治 <shaolizhi1234@126.com>
2020-04-17 16:29:11 +0800

    添加了 vuedragger 和 demo

--- src/views/target-ability/ability/Ability.vue
+++ src/views/target-ability/ability/Ability.vue
@@ -1,13 +1,57 @@
 <template>
-  <div class="container" v-hello>
-    ability
+  <div class="container">
+    <h1>Skills</h1>
+    <draggable v-model="myArray" ghost-class="ghost" @end="onEnd">
+      <transition-group type="transition" name="flip-list">
+        <div
+          class="sortable"
+          :id="element.id"
+          v-for="element in myArray"
+          :key="element.id"
+        >
+          <strong>{{ element.name }}</strong>
+          <span>{{ element.id }}</span>
+        </div>
+      </transition-group>
+    </draggable>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "@vue/composition-api";
+import { defineComponent, ref, reactive, watch } from "@vue/composition-api";
+import draggable from "vuedraggable";
 export default defineComponent({
-  setup() {}
+  setup() {
+    const myArray = ref([
+      { name: "Angular", id: 0 },
+      { name: "React", id: 1 },
+      { name: "Vue", id: 2 },
+      { name: "HTML", id: 3 },
+      { name: "CSS", id: 4 },
+      { name: "SASS", id: 5 }
+    ]);
+
+    watch(myArray, (newValue, oldValue) => {
+      console.log("list", newValue);
+    });
+
+    const oldIndex = ref("");
+    const newIndex = ref("");
+
+    const onEnd = function(event: any) {
+      console.log(event);
+      oldIndex.value = event.oldIndex;
+      newIndex.value = event.newIndex;
+    };
+
+    return {
+      myArray,
+      oldIndex,
+      newIndex,
+      onEnd
+    };
+  },
+  components: { draggable }
 });
 </script>
 
@@ -16,5 +60,34 @@ export default defineComponent({
   height 100%
   width 100%
   background white
+}
+strong {
+  display inline-block
+}
+.sortable {
+  width 100%
+  background white
+  padding 1em
+  cursor move
+  margin-bottom 2px
+}
+.container .sortable-drag {
+  // opacity 0
+}
+.flip-list-move {
+  transition transform 0.5s
+}
+.ghost {
+  border-left 6px solid rgb(0, 183, 255)
+  box-shadow 10px 10px 5px -1px rgba(0, 0, 0, 0.14)
+  opacity 0.7
+  &::before {
+    content ' '
+    position absolute
+    widows 20px
+    height 20px
+    margin-left -50px
+    background-image url('../../../assets/icon_add.svg')
+  }
 }
 </style>
