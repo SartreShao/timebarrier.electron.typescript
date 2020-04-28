8351fc47a50bd811a3fe59f18f471cdb8a9e317b
邵励治 <shaolizhi1234@126.com>
2020-04-28 21:53:08 +0800

    完成了 Ability 的事件绑定

--- src/views/target-ability/ability/Ability.vue
+++ src/views/target-ability/ability/Ability.vue
@@ -6,6 +6,7 @@
       v-for="ability in abilityList"
       :key="ability.id"
       :ability="ability"
+      v-longclick="() => click_abilityItem(ability)"
     ></ability-item>
     <tb-drawer isShow="ture">
       <tb-input></tb-input>
@@ -30,6 +31,7 @@ import { AbilityPage } from "@/lib/vue-viewmodels";
 import AbilityItem from "@/components/AbilityItem.vue";
 import TbInput from "@/lib/components/TbInput.vue";
 import TbDrawer from "@/lib/components/TbDrawer.vue";
+import { InputAbilityType } from "@/lib/types/vue-viewmodels";
 export default defineComponent({
   setup(props, context) {
     // 能力列表
@@ -40,6 +42,30 @@ export default defineComponent({
       Store.levelRuleList,
       ref([])
     );
+
+    // 控制变量：编辑能力抽屉
+    const isEditAbilityDrawerDisplayed: Ref<boolean> = inject(
+      Store.isEditAbilityDrawerDisplayed,
+      ref(false)
+    );
+
+    // 用户输入：编辑能力
+    const input_editingAbility: InputAbilityType = inject(
+      Store.input_editingAbility,
+      reactive(
+        reactive({
+          id: "",
+          name: "",
+          targetList: [],
+          planList: [],
+          isActived: true,
+          isFinished: false
+        })
+      )
+    );
+
+    // 点击事件：点击能力单项
+    const click_abilityItem = (ability: AV.Object) => {};
 
     // 生命周期：初始化
     onMounted(() => {
