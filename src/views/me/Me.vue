a712ce8220abd0fc87e7ccad8b012d7d960c999f
邵励治 <shaolizhi1234@126.com>
2020-05-04 11:16:35 +0800

    添加了 Me 界面的大部分 UI

--- src/views/me/Me.vue
+++ src/views/me/Me.vue
@@ -2,6 +2,76 @@
   <div class="container">
     <!-- 顶边栏 -->
     <top-bar></top-bar>
+
+    <!-- 头部布局 -->
+    <section class="header-container">
+      <me-personal-info
+        class="personal-info"
+        avatar-url="https://timebarrier.file.hearfresh.cn/4a90b2f4ea0c8130d78f/avatar.png"
+        :isVip="true"
+        name="邵励治"
+        description="shaolizhi@tipchou.com"
+      ></me-personal-info>
+    </section>
+
+    <!-- 睡眠、清醒与用时习惯 -->
+    <me-option
+      class="icon-sleep"
+      :icon="assets.icon_sleep"
+      name="睡眠、清醒与用时习惯"
+      description="8点起床"
+      :notification="0"
+      icon-width="4.41vw"
+      icon-height="2.44vh"
+      icon-margin-left="4.53vw"
+    ></me-option>
+
+    <!-- 成就与勋章 -->
+    <me-option
+      class="icon-regular"
+      :icon="assets.icon_achievement"
+      name="成就与勋章"
+      description="已达成 11 项成就"
+      :notification="0"
+      icon-width="3.83vw"
+      icon-height="2.60vh"
+      icon-margin-left="4.79vw"
+    ></me-option>
+
+    <!-- 清晨计划 -->
+    <me-option
+      class="icon-regular"
+      :icon="assets.icon_morning"
+      name="清晨计划"
+      description="暂未开放"
+      :notification="0"
+      icon-width="4.92vw"
+      icon-height="2.63vh"
+      icon-margin-left="4.27vw"
+    ></me-option>
+
+    <!-- 煤气灯酒吧 -->
+    <me-option
+      class="icon-regular"
+      :icon="assets.icon_bar"
+      name="煤气灯酒吧"
+      :notification="1"
+      icon-width="5.33vw"
+      icon-height="2.60vh"
+      icon-margin-left="4.07vw"
+    ></me-option>
+
+    <!-- 设置 -->
+    <me-option
+      class="icon-setting"
+      :icon="assets.icon_me_setting"
+      name="设置"
+      :notification="0"
+      icon-width="2.60vh"
+      icon-height="2.60vh"
+      icon-margin-left="4.41vw"
+    ></me-option>
+
     <!-- 底边栏 -->
     <bottom-bar></bottom-bar>
   </div>
@@ -10,14 +80,54 @@
 import { defineComponent } from "@vue/composition-api";
 import TopBar from "../../components/TopBar.vue";
 import BottomBar from "../../components/BottomBar.vue";
+import MeOption from "./components/MeOption.vue";
+import MePersonalInfo from "./components/MePersonalInfo.vue";
+import icon_sleep from "../../assets/icon_sleep.svg";
+import icon_achievement from "../../assets/icon_achievement.svg";
+import icon_morning from "../../assets/icon_morning.svg";
+import icon_bar from "../../assets/icon_bar.svg";
+import icon_me_setting from "../../assets/icon_me_setting.svg";
 export default defineComponent({
-  components: { TopBar, BottomBar }
+  components: { TopBar, BottomBar, MeOption, MePersonalInfo },
+  setup(props, context) {
+    return {
+      assets: {
+        icon_sleep,
+        icon_achievement,
+        icon_morning,
+        icon_bar,
+        icon_me_setting
+      }
+    };
+  }
 });
 </script>
 <style lang="stylus" scoped>
 .container {
+  width 100%
+  height 100vh
   display flex
   flex-direction column
-  background #f0f1f3
+  background #F5F5F5
+}
+.header-container {
+  margin-top 7.52vh
+  height 32.83vh
+  background white
+  width 100%
+  display flex
+  flex-direction column
+}
+.icon-sleep {
+  margin-top 3.45vh
+}
+.icon-regular {
+  margin-top 0.15vh
+}
+.icon-setting {
+  margin-top 2.02vh
+}
+.personal-info {
+  margin-top 3.45vh
 }
 </style>
---
+++ src/views/me/components/MePersonalInfo.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="me-personal-info-container">
+    <div class="avatar-qrcode-container">
+      <!-- 头像与 VIP -->
+      <div class="avatar-container">
+        <img :src="avatarUrl" alt="avatar" class="avatar" />
+        <img v-if="isVip" :src="assets.icon_vip" alt="icon_vip" class="vip" />
+      </div>
+
+      <!-- 二维码 -->
+      <img :src="assets.icon_qrcode" alt="icon_qrcode" class="qrcode" />
+    </div>
+
+    <div class="name">{{ name }}</div>
+
+    <div class="description">{{ description }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/composition-api";
+import icon_qrcode from "@/assets/icon_qrcode.svg";
+import icon_vip from "@/assets/icon_vip.svg";
+export default defineComponent({
+  props: {
+    avatarUrl: String,
+    isVip: Boolean,
+    qrcode: String,
+    name: String,
+    description: String
+  },
+  setup(props, context) {
+    return { assets: { icon_qrcode, icon_vip } };
+  }
+});
+</script>
+
+<style lang="stylus" scoped>
+.me-personal-info-container {
+  width 100%
+  display flex
+  flex-direction column
+  align-items center
+  .avatar-qrcode-container {
+    display flex
+    flex-direction row
+    justify-content center
+    align-items center
+    position relative
+    width 100%
+    .avatar-container {
+      position relative
+      height 8.55vh
+      .avatar {
+        width 8.55vh
+        height 8.55vh
+        border-radius 0.82vh
+        background gray
+      }
+      .vip {
+        position absolute
+        width 5.84vw
+        height 1.64vh
+        top -0.3vh
+        right -1.33vw
+      }
+    }
+    .qrcode {
+      width 2.12vh
+      height 2.12vh
+      position absolute
+      right 8.15vw
+      top 2.51vh
+    }
+  }
+  .name {
+    margin-top 2.47vh
+    font-size 2.25vh
+    font-weight bold
+    font-stretch normal
+    font-style normal
+    line-height 1.47
+    letter-spacing 0.02vh
+    text-align center
+    color #222a36
+  }
+  .description {
+    font-size 2.1vh
+    font-weight 300
+    font-stretch normal
+    font-style normal
+    line-height 1.43
+    letter-spacing 0.02vh
+    text-align left
+    color #222a36
+  }
+}
+</style>
