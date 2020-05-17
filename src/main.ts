ce7c6a1d447dda8cc7bc08fdbde64de706c08db7
邵励治 <shaolizhi1234@126.com>
2020-05-17 18:42:13 +0800

    删除了无效代码

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import VueCompositionApi from "@vue/composition-api";
-import * as AV from "leancloud-storage";
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 import Api from "./lib/api";
