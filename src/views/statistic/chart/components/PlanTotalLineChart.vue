b471f8f70e4fe8ef619f47587ab77a86b893affc
邵励治 <shaolizhi1234@126.com>
2020-06-20 00:44:47 +0800

    暂时修改了 PlanTotalLineChart 为「面积型」

--- src/views/statistic/chart/components/PlanTotalLineChart.vue
+++ src/views/statistic/chart/components/PlanTotalLineChart.vue
@@ -142,7 +142,7 @@ export default defineComponent({
         chartMode.value,
         colormap,
         true,
-        true
+        false
       );
     });
 
@@ -153,7 +153,7 @@ export default defineComponent({
         chartMode.value,
         colormap,
         true,
-        true
+        false
       );
     });
 
@@ -164,7 +164,7 @@ export default defineComponent({
         chartMode.value,
         colormap,
         true,
-        true
+        false
       );
     });
 
