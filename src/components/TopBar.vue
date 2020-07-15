d105015b0478057703eabb473f96ccaa37ac2620
邵励治 <shaolizhi1234@126.com>
2020-07-16 01:30:30 +0800

    完成了刷新功能

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -5,7 +5,7 @@
     <div style="height:1vh"></div>
     <!-- 标题与刷新按钮 -->
     <section class="title">
-      <div class="refresh">
+      <div class="refresh" @click="click_refreshDataButton">
         <img
           :src="assets.icon_logo"
           alt="icon_logo"
@@ -31,9 +31,11 @@
 import icon_logo from "../assets/icon_logo.svg";
 import icon_home from "../assets/icon_home.svg";
 import icon_back from "../assets/icon_back.svg";
-import { defineComponent, watch, ref, inject } from "@vue/composition-api";
+import { defineComponent, watch, ref, inject, Ref } from "@vue/composition-api";
 import Store from "@/store";
 import { Router } from "@/lib/vue-utils";
+import { App, StatPage } from "@/lib/vue-viewmodels";
+import AV from "leancloud-storage";
 
 export default defineComponent({
   setup(props, context) {
@@ -48,11 +50,121 @@ export default defineComponent({
     const click_homeButton = () => {
       Router.replace(context.root.$router, "/plan");
     };
+    // 下面请求数据
+    // Plan
+    // 服务器拉取的数据：临时计划的列表
+    const temporaryPlanList: Ref<AV.Object[]> = inject(
+      Store.temporaryPlanList,
+      ref<AV.Object[]>([])
+    );
+
+    // 服务器拉取的数据：每日计划的列表
+    const dailyPlanList: Ref<AV.Object[]> = inject(
+      Store.dailyPlanList,
+      ref<AV.Object[]>([])
+    );
+
+    // 服务器拉取的数据：已完成计划的列表
+    const completedPlanList: Ref<AV.Object[]> = inject(
+      Store.completedPlanList,
+      ref<AV.Object[]>([])
+    );
+
+    // Target
+    // 未分组的「目标」的列表
+    const unSubjectiveTargetList: Ref<AV.Object[]> = inject(
+      Store.unSubjectiveTargetList,
+      ref([])
+    );
+
+    //「目标类别」的列表
+    const targetSubjectList: Ref<AV.Object[]> = inject(
+      Store.targetSubjectList,
+      ref([])
+    );
+
+    // 已完成的「目标」列表
+    const completedTargetList: Ref<AV.Object[]> = inject(
+      Store.completedTargetList,
+      ref([])
+    );
+
+    // Ability
+    // 能力列表
+    const abilityList: Ref<AV.Object[]> = inject(Store.abilityList, ref([]));
+
+    // 能力等级列表
+    const levelRuleList: Ref<AV.Object[]> = inject(
+      Store.levelRuleList,
+      ref([])
+    );
+
+    // Statistic
+    // 用户选择的日期范围
+    const dateRange: Ref<Date[]> = inject(Store.dateRange, ref([]));
+
+    // 选择日期的提示语：如「15 日」、「30 日」、「180 日」
+    const dateTip: Ref<string> = inject(Store.dateTip, ref("15 日"));
+
+    // 番茄列表：带日期范围
+    const tomatoListWithDateRange: Ref<AV.Object[]> = inject(
+      Store.tomatoListWithDateRange,
+      ref([])
+    );
+
+    // StatisticTomato、StatisticTarget、StatisticPlan、StatisticAbility
+    const tomatoList: Ref<AV.Object[]> = inject(Store.tomatoList, ref([]));
+
+    // MultipleAnalyse
+    // 总时间
+    const totalTime: Ref<string> = inject(Store.totalTime, ref("0 小时"));
+
+    // 总番茄
+    const totalTomatoNumber: Ref<string> = inject(
+      Store.totalTomatoNumber,
+      ref("0 番茄")
+    );
+
+    // 本年的番茄列表
+    const thisYearTomatoList: Ref<AV.Object[]> = inject(
+      Store.thisYearTomatoList,
+      ref([])
+    );
+
+    // 点击事件：刷新按钮
+    const click_refreshDataButton = async () => {
+      await App.fetchAppData(
+        context.root,
+        temporaryPlanList,
+        dailyPlanList,
+        completedPlanList,
+        unSubjectiveTargetList,
+        targetSubjectList,
+        completedTargetList,
+        abilityList,
+        levelRuleList,
+        tomatoList,
+        totalTomatoNumber,
+        totalTime,
+        thisYearTomatoList
+      );
+
+      const startTime = dateRange.value[0];
+      const endTime = dateRange.value[1];
+      dateTip.value = StatPage.getDateTip(startTime, endTime);
+      StatPage.initTomatoListWithDateRange(
+        context.root,
+        tomatoListWithDateRange,
+        startTime,
+        endTime
+      );
+    };
 
     return {
       click_backButton,
       click_homeButton,
       isCurrentPageHome,
+      click_refreshDataButton,
       assets: { icon_logo, icon_home, icon_back }
     };
   }
