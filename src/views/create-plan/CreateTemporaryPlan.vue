757725818608d42e5e0757a7a0a6154bc8a084d2
邵励治 <shaolizhi1234@126.com>
2020-08-04 11:17:14 +0800

    完成了顶部的提示语

--- src/views/create-plan/CreateTemporaryPlan.vue
+++ src/views/create-plan/CreateTemporaryPlan.vue
@@ -5,8 +5,8 @@
 
     <!-- 主要页面 -->
     <section class="section">
-      <h1>创建一个「临时计划」</h1>
-      <h2>计划赶不上变化，临时计划帮我们应对变化</h2>
+      <h1 class="h-1">创建一个「临时计划」</h1>
+      <h2 class="h-2">计划赶不上变化，临时计划帮我们应对变化</h2>
     </section>
   </div>
 </template>
@@ -26,8 +26,28 @@ export default defineComponent({
   margin-top 7.52vh
   display flex
   flex-direction column
+  align-items center
   height 100%
   width 100%
   overflow scroll
+}
+
+.h-1 {
+  line-height 2.92vh
+  font-size 2.02vh
+  font-weight bold
+  color #222A36
+  margin-top 4.57vh
+  margin-bottom 0
+}
+
+.h-2 {
+  line-height 2.62vh
+  font-size 1.80vh
+  font-weight lighter
+  color #222A36
+  margin-top 1.20vh
+  opacity 0.8
+  margin-bottom 0
 }
 </style>
